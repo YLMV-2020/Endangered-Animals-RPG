3c8fe2c3d02fe50044855281feb782828dc1fdbb
Yordy Leonidas <yordy.lmv.2000@gmail.com>
2021-09-29 13:09:45 -0500

    Avance T1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+
+public class PlayerController: MonoBehaviour
+{
+    public static PlayerController sharedInstance;
+
+    [SerializeField] Camera cameraPlayer;
+    [SerializeField] FirstPersonController player;
+    [SerializeField] Animator animator;
+
+    Vector3 inputAxis;
+
+    private void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    void Start()
+    {
+        //scriptPersonController = GetComponent<FirstPersonController>();
+    }
+
+    void Update()
+    {
+
+        inputAxis = player.inputAxis;
+
+        animator.SetFloat("velocityX", inputAxis.x);
+        animator.SetFloat("velocityY", inputAxis.z);
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    
+
+
+    //void OnGUI()
+    //{
+    //    Debug.Log("gui evento");
+    //    GUI.Label(new Rect(new Vector2(20, 20), new Vector2(2,2)), "Game Over");
+    //}
+
+}
